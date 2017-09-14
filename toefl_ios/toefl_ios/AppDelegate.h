018e09a79dbc66f2ed977a5174d52abd3f5e9f33
wim <wim@wim-iMac.local>
2017-09-14 11:49:13 +0800

    first commit

---
+++ toefl_ios/toefl_ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  toefl_ios
+//
+//  Created by wim on 2017/9/14.
+//  Copyright © 2017年 wim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
